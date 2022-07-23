e963a762ae2b474700e4ed1200b844d4b5356d9e
Charles Xu <char8x@gmail.com>
2022-07-23 17:27:07 +0800

    update

--- process.ts
+++ process.ts
@@ -1,12 +1,12 @@
-const table = [{
-  name: "John",
-  age: 20,
-}, {
-  name: "Jane",
-  age: 21,
-}, {
-  name: "Joe",
-  age: 22,
-}];
+import { load } from 'https://esm.sh/cheerio@1.0.0-rc.12'
 
-console.table(table);
+const html = await fetch('https://www.alfredapp.com/workflows/').then(res => res.text());
+
+const $ = load(html);
+const pageText = $('#workflowspage > section:nth-child(4) > div > div > p:nth-child(1)').text().trim()
+
+if (pageText !== 'This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community.') {
+  console.log('Check it out!')
+} else {
+  console.log('This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community.')
+}
