c7c13d6720f336eb04190696aa1a2fad5633421a
Charles Xu <char8x@gmail.com>
2022-07-23 19:20:16 +0800

    update

--- process.ts
+++ process.ts
@@ -1,12 +1,40 @@
-import { load } from 'https://esm.sh/cheerio@1.0.0-rc.12'
+import { load } from "https://esm.sh/cheerio@1.0.0-rc.12";
 
-const html = await fetch('https://www.alfredapp.com/workflows/').then(res => res.text());
+async function sendMessage() {
+  const token = Deno.env.get("TELEGRAM_BOT_TOKEN") || "";
+  const chatId = Deno.env.get("TELEGRAM_CHAT_ID") || "";
 
-const $ = load(html);
-const pageText = $('#workflowspage > section:nth-child(4) > div > div > p:nth-child(1)').text().trim()
-
-if (pageText !== 'This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community.') {
-  console.log('Check it out!')
-} else {
-  console.log('This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community.')
+  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: "从 Github 发出消息!",
+    }),
+  });
 }
+
+await sendMessage()
+
+// const html = await fetch("https://www.alfredapp.com/workflows/").then((res) =>
+//   res.text()
+// );
+
+// const $ = load(html);
+// const pageText = $(
+//   "#workflowspage > section:nth-child(4) > div > div > p:nth-child(1)",
+// ).text().trim();
+
+// if (
+//   pageText !==
+//     "This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community."
+// ) {
+//   // TODO: send message to telegram
+
+// } else {
+//   console.log(
+//     "This page is currently being overhauled with a whole load of new amazing Workflows built by us and our community.",
+//   );
+// }
