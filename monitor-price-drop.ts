611683f6951b444a4d5a3a3d85492b5a58f2ce52
Charles Xu <char8x@gmail.com>
2022-08-02 16:51:38 +0800

    update env

--- monitor-price-drop.ts
+++ monitor-price-drop.ts
@@ -1,16 +1,35 @@
 import { load } from "cheerio";
 import { sendMessage } from "/utils.ts";
 
-const content = await fetch(
-  "https://www.miaomiaozhe.com/dvn/7726143786908001311",
-)
-  .then((res) => res.text());
-const $ = load(content);
-const priceText = $(
-  "#bd > div > div.panel > div > div.price-wrap.cl > div > span > strong",
-).text().trim();
-if (parseFloat(priceText) < 339) {
-  sendMessage("微波炉降价了，快去看");
-} else {
-  console.log(priceText);
+const SUCCESS_MSG = "降价了，快去看";
+
+async function trackPriceDrop(
+  url: string,
+  options: { itemTitle: string; currentPrice: number; successMsg?: string },
+) {
+  const { itemTitle, currentPrice, successMsg = SUCCESS_MSG } = options;
+  const content = await fetch(
+    url,
+  )
+    .then((res) => res.text());
+  const $ = load(content);
+  const priceText = $(
+    "#bd > div > div.panel > div > div.price-wrap.cl > div > span > strong",
+  ).text().trim();
+  if (parseFloat(priceText) < currentPrice) {
+    await sendMessage(`${itemTitle} ${successMsg}`);
+  } else {
+    console.log(`${itemTitle} ${priceText}`);
+  }
 }
+
+await Promise.all([
+  trackPriceDrop("https://www.miaomiaozhe.com/dvn/7726143786908001311", {
+    itemTitle: "微波炉",
+    currentPrice: 339,
+  }),
+  trackPriceDrop("https://www.miaomiaozhe.com/dvn/-3320863427635117050", {
+    itemTitle: "豆浆机",
+    currentPrice: 299.9,
+  }),
+]);
