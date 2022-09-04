3fce6eafa551d052fea585b5773c89be8e420eb6
Charles Xu <char8x@gmail.com>
2022-09-04 13:19:44 +0800

    feat: update

--- homebrew-deno.ts
+++ homebrew-deno.ts
@@ -1,22 +1,30 @@
 import { semver } from "./deps.ts";
 import { sendMessage } from "./utils.ts";
+import * as toml from "https://deno.land/std@0.154.0/encoding/toml.ts";
 
 const content = await fetch(
   "https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/deno.rb",
 ).then((res) => res.text());
 
-const onlineVersion = semver.valid(
+const homeBrewVersion = semver.valid(
   Array.from(
     // \/(.*?)\/ match content between / and /
     content.matchAll(/\/download\/(.*?)\/deno_src\.tar\.gz/g),
   ).flat().at(1) || "",
 );
-const localVersion = "1.24.3";
 
-console.log(`onlineVersion: ${onlineVersion}`);
-console.log(`localVersion: ${localVersion}`);
+const denoCargo = await fetch(
+  "https://raw.githubusercontent.com/denoland/deno/main/cli/Cargo.toml",
+).then((res) => res.text());
+const conf = toml.parse(denoCargo);
 
-if (semver.gt(onlineVersion || "", localVersion)) {
+// @ts-ignore
+const denoVersion = conf["package"]["version"];
+
+console.log(`homeBrewVersion: ${homeBrewVersion}`);
+console.log(`denoVersion: ${denoVersion}`);
+
+if (semver.eq(homeBrewVersion || "", denoVersion)) {
   // send msg
-  await sendMessage("Homebrew Deno 版本已升级，请及时更新");
+  await sendMessage("Homebrew Deno 版本已为最新，请及时更新");
 }
