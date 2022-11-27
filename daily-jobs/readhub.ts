b01fbebc107f678bfa2f610c47bbfc3962090ed5
Charles Xu <char8x@gmail.com>
2022-11-27 16:00:01 +0800

    update

--- daily-jobs/readhub.ts
+++ daily-jobs/readhub.ts
@@ -1,19 +1,31 @@
-import { Cheerio, Element, load } from "cheerio";
+import {
+  DOMParser,
+  Element,
+  initParser,
+} from "https://deno.land/x/deno_dom/deno-dom-wasm-noinit.ts";
 import { sendTGMessage } from "/utils.ts";
+
+await initParser();
+
 const URL = "https://readhub.cn/daily";
 
 const html = await fetch(URL).then((res) => res.text());
 
-const $ = load(html);
-const list = $(
-  `div[class^="Daily_main"] > div > div[class^="Daily_list"] > div:nth-child(n) > a`,
-).map((_, el) => {
-  const elem: Cheerio<Element> = $(el);
-  const title = elem.prop("innerText")?.replaceAll(/\./g, "\\.");
-  const href = elem.prop("href") as string;
+const doc = new DOMParser().parseFromString(
+  html,
+  "text/html",
+);
+
+const list = Array.from(
+  doc?.querySelectorAll(
+    `div[class^="Daily_main"] > div > div[class^="Daily_list"] > div:nth-child(n) > a`,
+  ) ?? [],
+).map((e) => {
+  const elem = e as Element;
+  const title = elem.innerText?.replaceAll(/\./g, "\\.");
+  const href = elem.getAttribute("href") ?? "";
   return `• [${title}](${href.startsWith("http") ? href : `${URL}${href}`})`;
-})
-  .toArray();
+});
 
 const message = `\\#Readhub每日早报 ${
   new Intl.DateTimeFormat("zh-CN").format(new Date()).replaceAll("/", "\\-")
